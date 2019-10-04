16841c58c1d4528b24aa9cef33e8571d5630b517
krystalkwan <krystal.m.kwan@gmail.com>
2019-10-04 11:09:58 -0700

    make file

--- TryAgain/src/theResult.java
+++ TryAgain/src/theResult.java
@@ -1,12 +1,16 @@
 public class theResult {
     public static void main(String [] args){
         int sum = 0;
+        int average = 0;
 
-        for(int i = 0; i < 21; i++){
-            System.out.print(i + " ");
-            sum += i;
+        for(int i = 21; i <= 31; i++){
+           System.out.print(i + " ");
+           sum += i;
+           average++;
         }
+        average = sum / average;
         System.out.println("\nThe sum is " + sum + ".");
+        System.out.println("The average is " + average + ".");
     }
 }
 // 1234/10=123
