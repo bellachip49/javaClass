8fd8ac813bfa0d95529c5b5627877fb514d3395d
bellachip49 <krystal.m.kwan@gmail.com>
2019-10-29 10:32:42 -0700

    add file

---
+++ krystalLesson_4-6-1_CW_Loops_Strings_2.java
@@ -0,0 +1,18 @@
+Lesson 4-6-1 CW Loops Strings 2
+//Krystal
+//10/29/19
+//Java
+
+Write code for each of the following problems.  Run it in IntelliJ.  Put the code and output here,
+then turn it in.
+
+1. Write code that produces the following output. Include a class with a main method.
+1 2 3 4 5
+6 7 8 9 10
+11 12 13 14 15
+
+2. Write class and main method with a while loop.  Make an array of int.  Take this number 3509723
+and put one digit in each box in the array.  Find the number of digits WITHOUT COUNTING THEM.  Find 
+the sum of the digits.
+
+3. Write code to make a 3 by 5 rectangle of @ signs.  Include a class and main method.
